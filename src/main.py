bd52fd283b9180486dbd9272140d88c071a9e365
Youssef Romanos <yromanos@gmail.com>
2026-05-17 17:36:42 +0400

    Add Flask web app with HTML page

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>My First App</title>
+        </head>
+        <body>
+            <h1>This is my first app written in VS Code and downloaded from GitHub</h1>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
